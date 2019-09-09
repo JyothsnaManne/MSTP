dc71982a68419652df41ab47367361698e6f469a
JyothsnaManne <54313236+JyothsnaManne@users.noreply.github.com>
2019-09-09 09:30:18 +0530

    Add files via upload

---
+++ arthimetic.py
@@ -0,0 +1,10 @@
+def operations(a,b):
+    print("addition:",a+b)
+    print("subtraction:",a-b)
+    print("multiplication:",a*b)
+    if(b==0):
+        print("not possible  by zero")
+    else:
+        print("division:",a/b)
+        print("remainder:",a%b)
+    
---
+++ mymodule.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+###### modules and packages in python
+### A python file contains function and python statements is called modules
+def welcome(name):
+    return "welcome" + name + "have a nice day"
+
